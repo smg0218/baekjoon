0404fe9b5d19502ad561ffa5fd682ef00666c3c3
smg0218 <128454773+smg0218@users.noreply.github.com>
2025-03-13 23:07:34 +0900

    [Bronze V] Title: A-B, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -4,11 +4,13 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         try {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            System.out.println(a - b);
+            bw.write(String.valueOf(a - b));
+            bw.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
