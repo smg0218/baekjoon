6e6bc4e5730d164106c3aa5c237eea6e77f69997
smg0218 <128454773+smg0218@users.noreply.github.com>
2025-04-07 19:42:24 +0900

    [Bronze IV] Title: 주사위 세개, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -9,14 +9,14 @@ public class Main {
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
 
-        if (a == b || a == c || b == c) {
-            if (a == b && b == c)
-                System.out.println(10000 + a * 1000);
-            else if(a == b)
+        if (a == b && b == c)
+            System.out.println(10000 + a * 1000);
+        else if (a == b || a == c || b == c) {
+            if (a == b)
                 System.out.println(1000 + a * 100);
             else
                 System.out.println(1000 + c * 100);
         } else
             System.out.println(Math.max(Math.max(a, b), Math.max(b, c)) * 100);
-        }
+    }
 }
