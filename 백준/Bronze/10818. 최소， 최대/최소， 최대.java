d6f5cbd4fe4d7ec0f2f8b7b55842587119cb8b9f
smg0218 <128454773+smg0218@users.noreply.github.com>
2025-04-09 12:59:11 +0900

    [Bronze III] Title: 최소, 최대, Time: 452 ms, Memory: 87984 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -4,9 +4,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int length = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine(), " ");
+        int length = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int value = Integer.parseInt(st.nextToken());
         int min = value;
         int max = value;
@@ -14,8 +13,8 @@ public class Main {
 
         for(int i = 1; i < length; i++) {
             value = Integer.parseInt(st.nextToken());
-            if(value < min) min = value;
-            if(value > max) max = value;
+            min = Math.min(min, value);
+            max = Math.max(max, value);
         }
 
         System.out.println(min + " " + max);
