f9d948b52ea5e349009a5dd7ee8f4c21d5ac7b12
smg0218 <128454773+smg0218@users.noreply.github.com>
2025-04-11 15:30:45 +0900

    [Silver V] Title: 너의 평점은, Time: 112 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -5,58 +5,47 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        float avg = 0.0f;
-        float sum = 0.0f;
-
+        double avg = 0.0f;
+        double sum = 0.0f;
 
         for(int i = 0; i < 20; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             st.nextToken();
             float score = Float.parseFloat(st.nextToken());
             String grade = st.nextToken();
-            if(grade.equals("P"))
-                continue;
-            switch (grade) {
-                case "A+":
-                    avg = (float) (avg + score * 4.5);
-                    sum += score;
-                    break;
-                case "A0":
-                    avg = (float) (avg + score * 4.0);
-                    sum += score;
-                    break;
-                case "B+":
-                    avg = (float) (avg + score * 3.5);
-                    sum += score;
-                    break;
-                case "B0" :
-                    avg = (float) (avg + score * 3.0);
-                    sum += score;
-                    break;
-                case "C+":
-                    avg = (float) (avg + score * 2.5);
-                    sum += score;
-                    break;
-                case "C0":
-                    avg = (float) (avg + score * 2.0);
-                    sum += score;
-                    break;
-                case "D+":
-                    avg = (float) (avg + score * 1.5);
-                    sum += score;
-                    break;
-                case "D0":
-                    avg = avg + score;
-                    sum += score;
-                    break;
-                case "F":
-                    avg = (float) (avg + score * 0.0);
-                    sum += score;
+            if(!grade.equals("P")) {
+                sum += score;
+                switch (grade) {
+                    case "A+":
+                        avg = avg + score * 4.5;
+                        break;
+                    case "A0":
+                        avg = avg + score * 4.0;
+                        break;
+                    case "B+":
+                        avg = avg + score * 3.5;
+                        break;
+                    case "B0":
+                        avg = avg + score * 3.0;
+                        break;
+                    case "C+":
+                        avg = avg + score * 2.5;
+                        break;
+                    case "C0":
+                        avg = avg + score * 2.0;
+                        break;
+                    case "D+":
+                        avg = avg + score * 1.5;
+                        break;
+                    case "D0":
+                        avg = avg + score;
+                        break;
+                    case "F":
+                        avg = avg + score * 0.0;
+                }
             }
         }
-        if(avg != 0.0)
-            avg = avg / sum;
 
-        System.out.printf(String.valueOf(avg));
+        System.out.printf(avg == 0.0 ? String.valueOf(avg) : String.valueOf(avg / sum));
     }
 }
