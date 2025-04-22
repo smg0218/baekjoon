424f8738c199e2f3d7d1ded4c78e8e909edd70f7
smg0218 <128454773+smg0218@users.noreply.github.com>
2025-04-22 17:36:00 +0900

    [Unrated] Title: Mystery, Time: 104 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Unrated/10212. Mystery/Mystery.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int N = (int)(Math.random()*2);
+        if(N%2 == 0)
+            System.out.print("Yonsei");
+        else
+            System.out.print("Korea");
+    }
+}
